1e1e63894b9d3451bf9caece350bdd99ef90d0b5
noahkise <noah@kisefamily.com>
2024-01-05 12:13:56 -0800

    add animations

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,13 @@ import KeyboardIcon from '@mui/icons-material/Keyboard';
 import EmailIcon from '@mui/icons-material/Email';
 import '../App.css';
 import headshot from '../assets/images/Screen Shot 2024-01-05 at 9.47.02 AM.png'
+import 'animate.css';
 
 function App() {
+
   return (
     <>
-      <div id='header'>
+      <div className="animate__animated animate__fadeInDown" id='header'>
         <div id='nameAndLinks'>
           <h1 id='name'>NoAH KISE</h1>
           <div id='links'>
@@ -38,11 +40,12 @@ function App() {
         </div>
         <img id='headshot' src={headshot} alt='head shot' />
       </div >
-      <hr className='line'></hr>
-      <p id='aboutMe'>Service industry professional stepping out from behind the bar into the dynamic world of web development.
-        Ready to apply a strong foundation of soft skills, multitasking, and quick thinking to a new career in tech.
-        Seeking a junior developer role that fosters continued growth to widen the breadth of my skills.</p>
-      <hr className='line'></hr>
+      <div className="animate__animated animate__backInUp">
+        <hr className='line'></hr>
+        <p id='aboutMe'>I'm a junior web developer and musician based in Portland, Oregon. Ready to apply my foundation of soft skills, multitasking, and quick thinking to a new career in tech. Seeking a junior developer role that encourages continued growth to widen the breadth of my experience.
+        </p>
+        <hr className='line'></hr>
+      </div>
     </>
   );
 }
