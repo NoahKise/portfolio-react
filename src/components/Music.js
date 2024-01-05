54b33cc37a405cfa4d95f31a5ce51ebaab4fc1d7
noahkise <noah@kisefamily.com>
2024-01-05 13:34:21 -0800

    music section added

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,5 +1,13 @@
 export const Music = () => {
-  return(
-    <h1>Music</h1>
+  return (
+    <>
+      <div className="animate__animated animate__backInUp">
+        <hr className='line'></hr>
+        <iframe
+          src="https://bandcamp.com/EmbeddedPlayer/album=550752137/size=large/bgcol=333333/linkcol=9a64ff/tracklist=false/transparent=true/"
+          seamless><a href="https://gumtooth.bandcamp.com/album/your-still-life">Your Still Life by Gumtooth</a></iframe>
+        <hr className='line'></hr>
+      </div>
+    </>
   )
 }
